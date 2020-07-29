404c56f88b440f3c540bf5edf96aaef03b7dfee7
JaredAppleman <65382344+JaredAppleman@users.noreply.github.com>
2020-07-29 00:01:51 -0500

    added some more buttons; progress on the draw button which is most of the game; tweaks to html and css

--- WarProjects/WarWebApp/assets/javascript/game.js
+++ WarProjects/WarWebApp/assets/javascript/game.js
@@ -5,12 +5,27 @@
 
 const p1CurrentCardHTML = document.querySelector('#p1CurrentCard');
 const p2CurrentCardHTML = document.querySelector('#p2CurrentCard');
+const messageHTML = document.querySelector('#message');
+const p1DeckLengthHTML = document.querySelector('#p1DeckLength')
+const p2DeckLengthHTML = document.querySelector('#p2DeckLength')
+const p1PileLengthHTML = document.querySelector('#p1PileLength')
+const p2PileLengthHTML = document.querySelector('#p2PileLength')
+let p1DECK = [];
+let p2DECK = [];
+let p1PILE = [];
+let p2PILE = [];
+
+let p1DECK_LENGTH = 0;
+let p2DECK_LENGTH = 0;
+let p1PILE_LENGTH = 0;
+let p2PILE_LENGTH = 0;
 
 class playingCard {
-    constructor(suit, value, weight, imageURL){
+    constructor(suit, value, suitWeight, deckWeight, imageURL){
         this.suit = suit;
         this.value = value;
-        this.weight = weight;
+        this.weight = suitWeight;
+        this.deckWeight = deckWeight
         this.imageURL = imageURL;
         this.name = value + ' of ' + suit + 's';
     }
@@ -19,53 +34,67 @@ class playingCard {
         const src = 'src="assets/images/PlayingCardsAlbum/' + this.imageURL + '.jpg"';
         return "<img " + src + " " + alt + ">";
     }
+    get abbreValue(){
+        if (! (this.value === '10')){
+            return this.value[0]
+        }
+        else{
+            console.log(this.value)
+            return this.value;
+        }
+    }
+    
 }
 
 
 
 const createShuffledDeck = function(){
+    //input: none
+    //output: deck
+    //doesn't change display
     const deck = [];
     const cardWeightList = [...Array(53).keys()]
     cardWeightList.splice(0,1)
     for (limit = 52; limit > 0; limit --){
         index = Math.floor(Math.random()*limit);
-        weight = cardWeightList[index];
+        deckWeight = cardWeightList[index];
         cardWeightList.splice(index,1);
-        if (weight <= 13){
+        if (deckWeight <= 13){
             suit = "Heart";
+            suitWeight = deckWeight;
         }
-        else if (weight <= 26){
+        else if (deckWeight <= 26){
             suit = "Spade";
-            weight = weight - 13;
+            suitWeight = deckWeight - 13;
         }
-        else if (weight <= 39){
+        else if (deckWeight <= 39){
             suit = "Diamond";
-            weight = weight - 26;
+            suitWeight = deckWeight - 26;
         }
         else{
             suit = "Club";
-            weight = weight - 39;
+            suitWeight = deckWeight - 39;
         }
 
-        if (weight === 13){
+        if (suitWeight === 13){
             value = "Ace";
         }
-        else if (weight === 12){
+        else if (suitWeight === 12){
             value = "King";
         }
-        else if (weight === 11){
+        else if (suitWeight === 11){
             value = "Queen";
         }
-        else if (weight === 10){
+        else if (suitWeight === 10){
             value = "Jack";
         }
         else{
-            value = weight + 1;
+            value = suitWeight + 1;
             value = value.toString();
         }
         imageURL = value + suit[0];
         //console.log(suit + ' ' + value)
-        let newCard = new playingCard(suit, value, weight, imageURL);
+        let newCard = new playingCard(suit, value, suitWeight, deckWeight, imageURL);
         //console.log(newCard.name)
         deck.push(newCard)
     }
@@ -74,6 +103,9 @@ const createShuffledDeck = function(){
 }
 
 const shuffleCards = function(deck){
+    //Input: deck
+    //Output: deck
+    //doesn't change dispaly
     shuffledDeck = [];
     deckLength = deck.length
 
@@ -86,12 +118,15 @@ const shuffleCards = function(deck){
     return shuffledDeck;
 }
 const dealCards = function(deck){
+    //Input: deck
+    //Output: 2 lists
+    //doesn't change display.
     player1 = true;
 
     const p1Deck = [];
     const p2Deck = [];
 
-    for (index = 0; index < deck.length; index++){
+    for (index = 0; index < deck.length;){
         card = deck.pop();
         if (player1){
             p1Deck.push(card);
@@ -102,25 +137,103 @@ const dealCards = function(deck){
             player1 = true;
         }
     }
+    console.log(p1Deck.length)
+    console.log(p2Deck.length)
     const output = [p1Deck, p2Deck];
 
     return output;
 }
     
+const updateGameInfo = function(){
 
+    p1DeckLengthHTML.innerHTML = 'Cards in Deck: ' + p1DECK.length;
+    p1PileLengthHTML.innerHTML = 'Cards in Pile: ' + p1PILE.length;
+    p2DeckLengthHTML.innerHTML = 'Cards in Deck: ' + p2DECK.length;
+    p2PileLengthHTML.innerHTML = 'Cards in Pile: ' + p2PILE.length;
+
+}
 
 
 const testFunction = function(){
+    //Input: none
+    //Output: 2 lists
+    // change from outline cards to back of cards; Draw button appears
     deck = createShuffledDeck();
+    console.log(deck.length)
     playerDecks = dealCards(deck);
-    p1Deck = playerDecks[0];
-    p2Deck = playerDecks[1];
+    p1DECK = playerDecks[0];
+    p2DECK = playerDecks[1];
+    p1DECK_LENGTH = p1DECK.length;
+    p2DECK_LENGTH = p2DECK.length;
+
+    updateGameInfo()
+    
     //alert("hello");
-    p1Card = p1Deck.pop();
-    p2Card = p2Deck.pop();
-    console.log(p1Card.suit)
-    //p1CurrentCardHTML.innerHTML =  currentCard.imageURL
-    p1CurrentCardHTML.innerHTML =  p1Card.imageHTML +'<p class="cardInfo">' + p1Card.name+ '</p>'
-    p2CurrentCardHTML.innerHTML =  p2Card.imageHTML +'<p class="cardInfo">' + p2Card.name+ '</p>'
 }
 
+const drawFunction = function(){
+    let p1Card = p1DECK.pop();
+    let p2Card = p2DECK.pop();
+    let cardPot = [p1Card, p2Card];
+
+    if (p1Card.weight > p2Card.weight){
+        p1PILE = p1PILE.concat(cardPot);
+        console.log("Player1 won the trick")
+        messageHTML.innerHTML = "Player1 Won the Trick"
+
+    }
+    else{
+        p2PILE = p2PILE.concat(cardPot)
+        console.log("Player2 won the trick")
+        messageHTML.innerHTML = "Player2 Won the Trick"
+    }
+    else(
+        console.log("there is a tie")
+
+        
+
+    )
+    
+    p1CurrentCardHTML.innerHTML =  p1Card.imageHTML +'<p class="cardInfo">' + p1Card.name+ '</p>'
+    p2CurrentCardHTML.innerHTML =  p2Card.imageHTML +'<p class="cardInfo">' + p2Card.name+ '</p>'
+    updateGameInfo()
+   // p1DeckLengthHTML.innerHTML
+}
+
+const tieFunction = function(){}
+
+const emptyDeck = function(deck){
+    
+}
+
+const displayCards = function(cardList){
+
+    let newLineBool = false;
+    let currentSuit = ''
+    let text = ''
+
+    if (cardList.length === 0){
+        alert("There are no cards.")
+    }
+
+    else{
+        cardList.sort((a,b) => {
+            return a.deckWeight - b.deckWeight});
+
+        for (index = 0; index < cardList.length; index++){
+            if (!(cardList[index].suit === currentSuit) && newLineBool){
+                text += '\n' + cardList[index].suit + ': ' + cardList[index].abbreValue;
+                currentSuit = cardList[index].suit;
+            }
+            else if (!(cardList[index].suit === currentSuit) && (!(newLineBool))){
+                text += cardList[index].suit + ': ' + cardList[index].abbreValue;
+                currentSuit = cardList[index].suit;
+                newLineBool = true;
+            }
+            else{
+                text += ', ' + cardList[index].abbreValue;
+            }
+        }
+        alert(text)
+    }
+}
